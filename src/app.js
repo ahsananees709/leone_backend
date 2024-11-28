9b4421502fdb767b0d9785a993a6fe66ec22b137
ahsananees709 <ahsanflower1@gmail.com>
2024-11-28 23:23:13 +0500

    2nd Commit

--- src/app.js
+++ src/app.js
@@ -4,22 +4,13 @@ import 'dotenv/config'
 import { SERVER_PORT, SERVER_HOST } from './utils/constant.js';
 import routes from "./routes/product.js"
 import { db } from '../db/db.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-// Get the current directory using `import.meta.url`
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = express()
 
 
 app.use(express.static("public"))
 app.use(express.json());
 app.use('/api',routes)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
-  });
+
 
 app.listen(SERVER_PORT,SERVER_HOST,() =>
 { 
